be2bfc2d0e68566909bc5efa1fe7efc308e701c8
Raketa <detroi.vv@gmail.com>
2018-03-10 16:28:39 +0200

    Create available rooms table

--- main/webapp/angular2/src/app/app.component.ts
+++ main/webapp/angular2/src/app/app.component.ts
@@ -10,20 +10,55 @@ export class AppComponent implements OnInit {
 
   public submitted: boolean;
   roomsearch: FormGroup;
+  rooms: Room[];
 
   ngOnInit() {
     this.roomsearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+
+    this.rooms = ROOMS;
   }
 
   onSubmit({value, valid}: { value: Roomsearch, valid: boolean }) {
     console.log(value);
   }
+
+  reserveRoom(value:string){
+    console.log("Room id for reservation:");
+  }
 }
 
-export interface Roomsearch{
+export interface Roomsearch {
   checkin: string;
   checkout: string;
 }
+
+export interface Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
+}
+
+var ROOMS:Room[] = [
+  {
+    "id":"38932123",
+    "roomNumber":"409",
+    "price":"20",
+    "links":""
+  },
+  {
+    "id":"83232323",
+    "roomNumber":"410",
+    "price":"25",
+    "links":""
+  },
+  {
+    "id":"1264554",
+    "roomNumber":"411",
+    "price":"28",
+    "links":""
+  }
+];
